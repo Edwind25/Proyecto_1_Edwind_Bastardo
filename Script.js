7e8bf5e814b6984d6773deeef335ff65c61bc3b3
Edwind <edwind2501@gmail.com>
2024-10-26 11:33:56 -0400

    se agrego un ciclo for el cual indica los numeros par e impar

--- Script.js
+++ Script.js
@@ -1,1 +1,10 @@
 console.log ("hola mundo")
+for (let numeros = 1; numeros <=20; numeros++) {
+
+    if (numeros % 2 === 0)
+        console.log (numeros+ "es Par")
+    else {
+        console.log (numeros+"es Impar")
+    }
+    
+}
