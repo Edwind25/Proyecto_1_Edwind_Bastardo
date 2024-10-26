de9ffa7661ed9596600add3d3c45513d3fd4fcdb
Edwind <edwind2501@gmail.com>
2024-10-26 11:56:52 -0400

    se agrego un botos que imprime los numeros par e impar

--- Script.js
+++ Script.js
@@ -1,10 +1,13 @@
 console.log ("hola mundo")
+function numerosParImpar () {
 for (let numeros = 1; numeros <=20; numeros++) {
 
-    if (numeros % 2 === 0)
+    if (numeros % 2 == 0)
         console.log (numeros+ "es Par")
     else {
         console.log (numeros+"es Impar")
     }
     
+}
+
 }
